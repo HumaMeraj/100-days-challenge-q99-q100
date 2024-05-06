00c4f3c7540f16646ed1d342a4459a0a7fbc3810
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-07 01:32:22 +0500

    File of q100

---
+++ q100.js
@@ -0,0 +1,5 @@
+//Use the JavaScript Math object to find the square root of 144.
+// Finds the square root of 144 using Math.sqrt()
+var squareRoot = Math.sqrt(144);
+console.log(squareRoot); // Outputs: 12
+// This line effectively calculates and shows the square root of 144.
